e7e32ac6901be5375b34ec141b3b02f185a0ac33
Alan Cowap <spam@alancowap.com>
2015-04-16 11:12:04 +0100

    Add DrawOvalApplet

---
+++ MyFirstApplet/src/com/alancowap/cag/applet/DrawOvalApplet.java
@@ -0,0 +1,45 @@
+package com.alancowap.cag.applet;
+// This class adapted from: http://www.java-examples.com/draw-oval-circle-applet-window-example
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
+public class DrawOvalApplet extends JApplet {
+
+	/**
+	 * Create the applet.
+	 */
+	public DrawOvalApplet() {
+
+	}
+	public void paint(Graphics g){
+
+		//set color to red
+		setForeground(Color.red);
+
+		/*
+		 * to draw a oval in an applet window use,
+		 * void drawOval(int x1,int y1, int width, int height)
+		 * method.
+		 *
+		 * This method draws a oval of specified width and
+		 * height at (x1,y1)
+		 */
+
+		//this will draw a oval of width 50 & height 100 at (10,10)
+		g.drawOval(10,10,50,100);
+
+
+		/*
+		 * To draw a filled oval use
+		 * fillOval(int x1,int y1, int width, int height)
+		 * method of Graphics class.
+		 */
+
+		//draw filled oval
+		g.fillOval(100,20,50,100);
+
+
+	}
+
+}
