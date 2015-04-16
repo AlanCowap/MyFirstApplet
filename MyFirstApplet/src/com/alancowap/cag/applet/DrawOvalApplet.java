af15ada61d8f23735261da0cd64f05d55aed6654
Alan Cowap <spam@alancowap.com>
2015-04-16 11:22:13 +0100

    Add blue circle

--- MyFirstApplet/src/com/alancowap/cag/applet/DrawOvalApplet.java
+++ MyFirstApplet/src/com/alancowap/cag/applet/DrawOvalApplet.java
@@ -39,6 +39,10 @@ public class DrawOvalApplet extends JApplet {
 		//draw filled oval
 		g.fillOval(100,20,50,100);
 
+		// Note that a circle is an oval where width = height
+		// let's draw a blue circle
+		g.setColor(Color.blue);
+		g.drawOval(70, 100, 20, 20);
 
 	}
 
